be52386049aa205134d0845cd1c3b4abc15b0ada
zabed <zabedalam777@gmail.com>
2021-03-29 15:41:16 +0200

    menu box done

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../../components/header";
+import Menu from "../../components/menu";
 
 function LandingPage() {
   return (
     <div className="main-container">
+      {/* Header */}
       <Header />
+      {/* Menu */}
+      <div className="left-container container">
+        <Menu />
+      </div>
     </div>
   );
 }
