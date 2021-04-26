c69ee59295aa3e0532a708164fd32cbddb8b263b
Shanawaz Islam <acarif@gmail.com>
2021-04-27 00:47:40 +0200

    Added app use req res next

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ const buildPath = path.join(__dirname, 'dist');
 app.use(express.static(buildPath));
 app.set('port', process.env.PORT || 8080);
 
+app.use((err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.httpStatus);
+    if (err.body) {
+      return res.json(err.body);
+    }
+    return res.send({ error: err.message });
+  }
+  res.sendStatus(500);
+});
+
 app.use('*', (req, res) => {
   res.sendFile(path.join(`${buildPath}/index.html`));
 });
