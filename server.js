3320fb473b8fac234ed659ba9999ae1e425ce6d3
Shanawaz Islam <acarif@gmail.com>
2021-04-26 17:12:34 +0200

    Added server listen on error

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const debug = require('debug')('http');
 
 const app = express();
 const buildPath = path.join(__dirname, 'dist');
@@ -11,6 +13,39 @@ app.use('*', (req, res) => {
   res.sendFile(path.join(`${buildPath}/index.html`));
 });
 
-var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
-});
+const server = http.createServer(app);
+
+const port = app.get('port');
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+  console.log(
+    `Server Listening on ${bind}`,
+  );
+}
