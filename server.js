0a2d3c97a263e3b2e131b1869351350209a37c8e
Shanawaz Islam <acarif@gmail.com>
2021-04-26 16:53:11 +0200

    Added app use html

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-var path = require('path');
-var express = require('express');
+const path = require('path');
+const express = require('express');
 
-var app = express();
+const app = express();
+const buildPath = path.join(__dirname, 'dist');
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(buildPath));
 app.set('port', process.env.PORT || 8080);
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(`${buildPath}/index.html`));
+});
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
